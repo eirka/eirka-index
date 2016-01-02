e032c6cb0a9dc1f3e29566fb6cf7c7c961e589b3
techjanitor <puntme@gmail.com>
2016-01-01 18:15:05 -0600

    loop through imageboards for auto nav menu

--- templates.go
+++ templates.go
@@ -78,8 +78,4 @@ const header = `[[define "header"]]<div class="header_bar">
     </div>
 </div>[[end]]`
 
-const navmenu = `[[define "navmenu"]]
-[[ range $ib := .imageboards ]]
-<li><a href="//[[ $ib.Address ]]">[[ $ib.Title ]]</a></li>
-[[end]]
-[[end]]`
+const navmenu = `[[define "navmenu"]][[ range $ib := .imageboards ]]<li><a href="//[[ $ib.Address ]]">[[ $ib.Title ]]</a></li>[[end]][[end]]`

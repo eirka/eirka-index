583a371336514de41f6fca8483804c0765317c64
techjanitor <puntme@gmail.com>
2016-02-19 23:37:25 -0600

    move stylesheets above js

--- templates.go
+++ templates.go
@@ -20,10 +20,8 @@ const head = `[[define "head"]]<head>
 <meta charset="utf-8" />
 <meta name="viewport" content="width=device-width, initial-scale=1" />
 <meta name="description" content="[[ .desc ]]" />
-[[if .nsfw -]]
-<meta name="rating" content="adult" />
-<meta name="rating" content="RTA-5042-1996-1400-1577-RTA" />
-[[- end]]
+[[if .nsfw -]]<meta name="rating" content="adult" />
+<meta name="rating" content="RTA-5042-1996-1400-1577-RTA" />[[- end]]
 <link rel="stylesheet" href="/assets/prim/[[ .primcss ]]" />
 <link rel="stylesheet" href="/assets/styles/[[ .style ]]" />
 <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.4.0/css/font-awesome.min.css">
@@ -34,63 +32,61 @@ const head = `[[define "head"]]<head>
 
 // angular config
 const angular = `[[define "angular"]]<script>angular.module('prim').constant('config',{
-    ib_id:[[ .ib ]],
-    title:'[[ .title ]]',
-    img_srv:'//[[ .imgsrv ]]',
-    api_srv:'//[[ .apisrv ]]',
-    csrf_token:'[[ .csrf ]]'
+ib_id:[[ .ib ]],
+title:'[[ .title ]]',
+img_srv:'//[[ .imgsrv ]]',
+api_srv:'//[[ .apisrv ]]',
+csrf_token:'[[ .csrf ]]'
 });
 </script>[[end]]`
 
 // site header
 const header = `[[define "header"]]<div class="header_bar">
-    <div class="left">
-        <div class="nav_menu" ng-controller="NavMenuCtrl as navmenu">
-            <ul click-off="navmenu.close" ng-click="navmenu.toggle()" ng-mouseenter="navmenu.open()" ng-mouseleave="navmenu.close()">
-                <li class="n1"><a href><i class="fa fa-fw fa-bars"></i></a>
-                    <ul ng-if="navmenu.visible">
-                        [[template "navmenuinclude" . ]]
-                        [[template "navmenu" . ]]
-                    </ul>
-                </li>
-            </ul>
-        </div>
-        <div class="nav_items" ng-controller="NavItemsCtrl as navitems">
-            <ul>
-                <ng-include src="'pages/menus/nav.html'"></ng-include>
-            </ul>
-        </div>
-    </div>
-    <div class="right">
-        <div class="user_menu">
-            <div ng-if="!authState.isAuthenticated" class="login">
-                <a href="account" class="button-login">Sign in</a>
-            </div>
-            <div ng-if="authState.isAuthenticated" ng-controller="UserMenuCtrl as usermenu">
-                <ul click-off="usermenu.close" ng-click="usermenu.toggle()" ng-mouseenter="usermenu.open()" ng-mouseleave="usermenu.close()">
-                    <li>
-                        <div class="avatar avatar-medium">
-                            <div class="avatar-inner">
-                                <a href>
-                                    <img ng-src="{{authState.avatar}}" />
-                                </a>
-                            </div>
-                        </div>
-                        <ul ng-if="usermenu.visible">
-                            <ng-include src="'pages/menus/user.html'"></ng-include>
-                        </ul>
-                    </li>
-                </ul>
-            </div>
-        </div>
-        <div class="site_logo">
-            <a href="/[[ .base ]]">
-                <img src="/assets/logo/[[ .logo ]]" title="[[ .title ]]" />
-            </a>
-        </div>
-    </div>
+<div class="left">
+<div class="nav_menu" ng-controller="NavMenuCtrl as navmenu">
+<ul click-off="navmenu.close" ng-click="navmenu.toggle()" ng-mouseenter="navmenu.open()" ng-mouseleave="navmenu.close()">
+<li class="n1"><a href><i class="fa fa-fw fa-bars"></i></a>
+<ul ng-if="navmenu.visible">
+[[template "navmenuinclude" . ]]
+[[template "navmenu" . ]]
+</ul>
+</li>
+</ul>
+</div>
+<div class="nav_items" ng-controller="NavItemsCtrl as navitems">
+<ul>
+<ng-include src="'pages/menus/nav.html'"></ng-include>
+</ul>
+</div>
+</div>
+<div class="right">
+<div class="user_menu">
+<div ng-if="!authState.isAuthenticated" class="login">
+<a href="account" class="button-login">Sign in</a>
+</div>
+<div ng-if="authState.isAuthenticated" ng-controller="UserMenuCtrl as usermenu">
+<ul click-off="usermenu.close" ng-click="usermenu.toggle()" ng-mouseenter="usermenu.open()" ng-mouseleave="usermenu.close()">
+<li>
+<div class="avatar avatar-medium">
+<div class="avatar-inner">
+<a href>
+<img ng-src="{{authState.avatar}}" />
+</a>
+</div>
+</div>
+<ul ng-if="usermenu.visible">
+<ng-include src="'pages/menus/user.html'"></ng-include>
+</ul>
+</li>
+</ul>
+</div>
+</div>
+<div class="site_logo">
+<a href="/[[ .base ]]">
+<img src="/assets/logo/[[ .logo ]]" title="[[ .title ]]" />
+</a>
+</div>
+</div>
 </div>[[end]]`
 
-const navmenu = `[[define "navmenu"]][[ range $ib := .imageboards -]]
-<li><a target="_self" href="//[[ $ib.Address ]]/">[[ $ib.Title ]]</a></li>
-[[- end]][[end]]`
+const navmenu = `[[define "navmenu"]][[ range $ib := .imageboards -]]<li><a target="_self" href="//[[ $ib.Address ]]/">[[ $ib.Title ]]</a></li>[[- end]][[end]]`
